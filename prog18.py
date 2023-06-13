c75725b5313fd47e6d66028eafe9b72fa998f016
shelkeom230 <shelkeom230@gmail.com>
2023-06-13 19:03:51 +0530

    sets programs added

--- prog18.py
+++ prog18.py
@@ -1,1 +1,18 @@
 # python sets 
+# unordred, unchangable and unindexed 
+
+thisset={'apple','banana','kiwi'}; print(thisset)
+
+for x in thisset: print(x)
+print(len(thisset))
+
+thisset=set(('apple','watermelon','orange')); print(thisset)
+print('watermelon' in thisset)
+
+thisset.add('orange'); print(thisset)
+
+tropical={'pineapple','mango','papaya'}
+mylist=['kiwi','ornage']
+thisset.update(mylist); print(thisset)
+
+mylist.discard('pineapple'); print(mylist)
